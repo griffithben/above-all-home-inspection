df22c58ac77fc7e6b7ca0b0089c782f1126d3322
Benjamin Griffith <griffith.ben@gmail.com>
2018-09-02 23:34:24 -0500

    modifications

--- resources/views/layouts/primary.blade.php
+++ resources/views/layouts/primary.blade.php
@@ -11,5 +11,61 @@
     </head>
     <body>
 
+        <!-- Navigation -->
+        @include('partials.nav')
+
+        <header>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <!-- Slide One - Set the background image for this slide in the line below -->
+                    <div class="carousel-item active" style="background-image: url('/img/slider-1.jpg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>First Slide</h3>
+                            <p>This is a description for the first slide.</p>
+                        </div>
+                    </div>
+                    <!-- Slide Two - Set the background image for this slide in the line below -->
+                    <div class="carousel-item" style="background-image: url('/img/slider-2.jpg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>Second Slide</h3>
+                            <p>This is a description for the second slide.</p>
+                        </div>
+                    </div>
+                    <!-- Slide Three - Set the background image for this slide in the line below -->
+                    <div class="carousel-item" style="background-image: url('/img/slider-3.jpg')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>Third Slide</h3>
+                            <p>This is a description for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </header>
+
+        <!-- Page Content -->
+        <section class="py-5">
+        <div class="container">
+            <h1>Protect Your Future</h1>
+            <p>The background images for the slider are set directly in the HTML using inline CSS. The rest of the styles for this template are contained within the
+            <code>full-slider.css</code>
+            file.</p>
+        </div>
+        </section>
+
+        @include('partials.footer')
+        
     </body>
 </html>
