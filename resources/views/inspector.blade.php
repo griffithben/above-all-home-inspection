a9bc632074d28fdfcaa7e10345a559fe048854f3
Benjamin Griffith <griffith.ben@gmail.com>
2018-09-25 08:33:29 -0500

    more content

--- resources/views/inspector.blade.php
+++ resources/views/inspector.blade.php
@@ -5,12 +5,20 @@
 @php $menu = 'inspector' @endphp
 
 @section('content')
-    <section class="py-5">
+<section class="py-5">
         <div class="container">
-            <h1>Inspector</h1>
-            <p>The background images for the slider are set directly in the HTML using inline CSS. The rest of the styles for this template are contained within the
-            <code>full-slider.css</code>
-            file.</p>
+            <div class="row">
+                <div class="col-sm-3">
+                <div class="text-center">
+                    <img src="/img/headshot.jpg" class="rounded-circle w-100" alt="...">
+                </div>
+                </div>
+                <div class="col-sm-9">
+                    <h1>Kevin Marshall</h1>
+                    <p>Whether you're buying, selling or own a home, having a professional home inspection will give you peace of mind and assist you in knowing the health, safety, and condition of your home. I take great pride in every inspection i perform. When the inspection is over, your home inspector will prepare a detailed inspection report regarding the property. Most Inspection reports will be delivered same day via email.</p>
+                    <p>As a certified home inspector, I spend on average 3 hours on most inspections and provide a detailed report including pictures with over 400 items inspected.</p>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -7,10 +7,40 @@
 @section('content')
     <section class="py-5">
         <div class="container">
-            <h1>Services</h1>
-            <p>The background images for the slider are set directly in the HTML using inline CSS. The rest of the styles for this template are contained within the
-            <code>full-slider.css</code>
-            file.</p>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>Services</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item bg-transparent">Roof, Vents, Flashings and Trim, Gutters, and Downspouts</li>
+                        <li class="list-group-item bg-transparent">Skylight, Chimney, and other Roof penetrations </li>
+                        <li class="list-group-item bg-transparent">Eaves, Soffits, and Fascia</li>
+                        <li class="list-group-item bg-transparent">Decks, Stoops</li>
+                        <li class="list-group-item bg-transparent">Porches, Walkways, and Railings</li>
+                        <li class="list-group-item bg-transparent">Grading and Drainage</li>
+                        <li class="list-group-item bg-transparent">Basement, Foundation, and Crawlspace</li>
+                        <li class="list-group-item bg-transparent">Water penetration and Foundation movement</li>
+                        <li class="list-group-item bg-transparent">Heating systems, Cooling systems</li> 
+                        <li class="list-group-item bg-transparent">Main water shut off Valves</li>
+                    </ul>
+                </div>
+                <div class="col-sm">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item bg-transparent">Water heating system</li>
+                        <li class="list-group-item bg-transparent">Interior plumbing Fixtures and Faucets </li> 
+                        <li class="list-group-item bg-transparent">Drainage sump pumps with accessible floats</li>
+                        <li class="list-group-item bg-transparent">Electrical Service Line and Meter Box</li>
+                        <li class="list-group-item bg-transparent">Main Disconnects and Service Amperage</li>
+                        <li class="list-group-item bg-transparent">Electrical panels, Breakers and Fuses</li>
+                        <li class="list-group-item bg-transparent">Grounding and Bonding, GFCIs and AFCIs</li>
+                        <li class="list-group-item bg-transparent">Garage Doors, Safety Sensors, and Openers</li>
+                        <li class="list-group-item bg-transparent">And much, more</li> 
+                    </ul>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
