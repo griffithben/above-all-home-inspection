f11c24efd66a307b7760c8e45b6bb4eb5d770722
Benjamin Griffith <griffith.ben@gmail.com>
2020-03-15 20:55:54 -0500

    update footer email

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-5">
     <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; Above All Home Inspection 2020</p>
-        <p class="m-0 text-center text-white">‭573-378-0351‬ | kevin@aboveallhomeinspection.com</p>
+        <p class="m-0 text-center text-white">‭573-378-0351‬ | kevin@aboveallhomeinspectionllc.com</p>
     </div>
 </footer>
