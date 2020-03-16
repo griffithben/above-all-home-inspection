3b08c827057ead9061b4785bd3b38408dcc123cf
Benjamin Griffith <griffith.ben@gmail.com>
2020-03-15 20:46:33 -0500

    sendgrid wireup

--- app/Http/Controllers/ContactRequestController.php
+++ app/Http/Controllers/ContactRequestController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PricingRequest;
 use Illuminate\Http\Request;
-use SendGrid;
-use SendGrid\Mail\Mail;
 
 class ContactRequestController extends Controller
 {
@@ -13,20 +11,26 @@ class ContactRequestController extends Controller
     {
         $validated = $pricingRequest->validated();
 
-        $email = new Mail(
-            $validated['email'],
-            'griffith.ben@gmail.com',
-            'Quote Requested From '.$validated['first_name'].' '.$validated['last_name']
-        );
-        
+        $email = new \SendGrid\Mail\Mail();
+        $email->setFrom("website@aboveallhomeinspection.com", "Online Quote");
+        $email->setSubject('Quote Requested From '.$validated['first_name'].' '.$validated['last_name']);
+        $email->addTo('griffith.ben@gmail.com', "Ben Griffith");
         $email->addContent(
-            "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+            "text/html",
+            "<span><strong>Name:</strong> ".$validated['first_name']." ".$validated['last_name']."</span><br/>
+            <span><strong>Email:</strong> ".$validated['email']."</span><br/>
+            <span><strong>Phone:</strong> ".$validated['phone']."</span><br/>
+            <span><strong>Approx. SqFt:</strong> ".$validated['approximate_square_feet']."</span><br/>
+            <span><strong>Comments:</strong> ".$validated['comments']."</span>"
         );
-
-        $sendgrid = new SendGrid(env('SENDGRID_API_KEY'));
-        $sendgrid->send($email);
-
-        $request->session()->flash('success', 'Request has been sent!');
+        $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+        print_r($validated);
+        try {
+            $response = $sendgrid->send($email);
+            $request->session()->flash('success', 'Request has been sent!');
+        } catch (\Exception $e) {
+            echo 'Caught exception: '. $e->getMessage() ."\n";
+        }
 
         return redirect()->to('/contact');
     }
